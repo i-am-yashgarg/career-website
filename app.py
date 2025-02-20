424b8c06de7f9a20065dd19ea308d0592d5f1899
1yashgarg00 <39835314-1yashgarg00@users.noreply.replit.com>
2025-02-20 09:43:32 +0000

    User checkpoint: Fix: Correct app.py execution and add replit.nix dependencies.

--- app.py
+++ app.py
@@ -1,11 +1,1 @@
-
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "Hello! Welcome to the Career Website"
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=3000)
+print("hello")
